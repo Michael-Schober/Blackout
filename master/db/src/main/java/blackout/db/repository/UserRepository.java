3c0fb853a12adbc009b33bd903666cb253d049da
Michael-Schober <40370288+Michael-Schober@users.noreply.github.com>
2020-01-07 09:37:40 +0100

    delete obsolete

--- master/db/src/main/java/blackout/db/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package blackout.db.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import Blackout.shared.model.OauthUser;
-
-public interface UserRepository extends JpaRepository<OauthUser, Long>
-{
-    public OauthUser getByUsername(String username);
-}
